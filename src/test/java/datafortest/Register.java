c58f535009b14dddc6bed91d3d4e45db80efe267
unknown <mak@cron.ooo>
2022-09-29 21:34:12 +0300

    added tests

---
+++ src/test/java/datafortest/Register.java
@@ -0,0 +1,7 @@
+package datafortest;
+
+public abstract class Register {
+    public static final String NAME = "Ivan";
+    public static final String PASSWORD = "test_password";
+    public static final String INVALID_PASSWORD = "12345";
+}
